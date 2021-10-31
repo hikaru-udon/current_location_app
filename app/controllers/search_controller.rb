b5798baa4f47d92c571a825801025d3c26df2a2e
hikaru-udon <mare.nozomu512@gmail.com>
2021-10-31 12:58:06 +0900

    xml形式のデータをJson形式に変更

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,14 +5,19 @@ class SearchController < ApplicationController
     # puts '---------'
     # puts params[:shop_name]
     if shop_name = params[:shop_name]
-      params = URI.encode_www_form({keyword: shop_name})
+      params = URI.encode_www_form({name: shop_name})
       uri = URI.encode("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=261324dd569a9418&name=#{shop_name}")
-      # puts uri.class
+      puts "-------"
+      puts params
+      puts "-------"
       uri = URI.parse(uri)
       response = Net::HTTP.get_response(uri)
-      json = response.body
+      #xml形式で受け取っているので、json形式に変化する
+      json = response.to_json
       result = JSON.parse(json)
+      puts "-------"
       puts result
+      puts "-------"
       if result["results"]
         @shop_name = result["results"][0]["name"]
       end
