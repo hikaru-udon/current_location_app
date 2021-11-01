955ce2d6db3bf43ee3210f505df552404858feee
hikaru-udon <mare.nozomu512@gmail.com>
2021-11-01 10:45:04 +0900

    search#indexをトップページへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'search', to: "search#index"
+  root "search#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
