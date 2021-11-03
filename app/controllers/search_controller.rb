ad55eabb81fc6eb67c61d30680c85d7ba7a82a61
hikaru-udon <mare.nozomu512@gmail.com>
2021-11-04 06:50:06 +0900

    店舗名検索に基づいたリクエストとレスポンス機能を実装
    ・入力情報の受け取り
    ・Json形式のデータをHash形式に変換して読み取り
    ・@shopsとしてインスタンス変数化

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,15 +3,14 @@ class SearchController < ApplicationController
   def index
     require 'net/http'
 
-    if shop_name = params[:shop_name]
+    if shop_name = params[:shop_name] 
       params = URI.encode_www_form({name: shop_name})
-      uri = URI.encode("https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=261324dd569a9418&name=#{shop_name}&count=10&format=json")
-      uri = URI.parse(uri)
+      encode_uri = URI.encode("https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=261324dd569a9418&name=#{shop_name}&count=100&format=json")
+      uri = URI.parse(encode_uri)
       response = Net::HTTP.get_response(uri)
-      #Jsonデータを解析し、Rubyで取り出すためにHash形式に変換
+      #Json形式のデータを解析し、Rubyで取り出すためにHash形式に変換
       hash = JSON.parse(response.body)
       @shops = hash["results"]["shop"]
-      # binding.pry
     end
 
   end
